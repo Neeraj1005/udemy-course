8b88713df2afb87260d19d885c1c1e5e99f63689
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-04-24 16:37:23 +0530

    event page added

---
+++ next-fundamental/pages/events/add.js
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout'
+
+export default function AddEventPage() {
+  return (
+    <Layout title='Add New Event'>
+      <h1>Add Event</h1>
+    </Layout>
+  )
+}
+© 2021 GitHub, Inc.

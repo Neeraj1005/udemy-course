a782d2588bcb36fbf96cbfb9ec020aedf1e9c5f7
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-04-27 20:20:19 +0530

    button component added with events

--- react-crash-course/src/components/Header.js
+++ react-crash-course/src/components/Header.js
@@ -1,24 +1,24 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
-    return (
-        <header>
-            <h1 style={headingStyle}>{title}</h1>
-        </header>
-    )
-}
+  const onClick = () => {
+    console.log("cllicked");
+  };
+  return (
+    <header>
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-// css in js
-const headingStyle = {
-    'color': 'red',
-    'background-color': 'blue'
-}
-export default Header
+export default Header;

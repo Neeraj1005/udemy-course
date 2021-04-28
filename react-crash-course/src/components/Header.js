cae79c929a82c6a4b077487afabfa5f1e0df0ef5
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-04-28 11:41:09 +0530

    additional option added in toggle button

--- react-crash-course/src/components/Header.js
+++ react-crash-course/src/components/Header.js
@@ -1,12 +1,11 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title, onAdd }) => {
-  console.log(onAdd)
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header>
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onAdd} />
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
     </header>
   );
 };

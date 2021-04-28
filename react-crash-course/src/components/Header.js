87fb7a3da1cdf4eb4da0932c932025260afad0d5
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-04-28 11:35:59 +0530

    toggle add button added

--- react-crash-course/src/components/Header.js
+++ react-crash-course/src/components/Header.js
@@ -1,14 +1,12 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log("cllicked");
-  };
+const Header = ({ title, onAdd }) => {
+  console.log(onAdd)
   return (
     <header>
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
+      <Button color="green" text="Add" onClick={onAdd} />
     </header>
   );
 };

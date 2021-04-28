520673a64e236d78f9aebfcea2bc5e1994bc16bf
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-04-28 19:34:07 +0530

    fetch from server and delete

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -1,25 +1,44 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
 
 function App() {
-
-  const [showAddTask, setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTask = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTask()
+  }, []);
+
+  // Fetch Task
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
 
   // Add Task
   const addTask = (task) => {
     // console.log(task)
     const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task }
+    const newTask = { id, ...task };
 
-    setTasks([...tasks, newTask])
-  }
+    setTasks([...tasks, newTask]);
+  };
 
   // Delete Task
-  const deleteTask = (id) => {
-    // console.log('delete', id)
+  const deleteTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    });
+
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
@@ -33,10 +52,11 @@ function App() {
   };
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && (
-        <AddTask onAdd={addTask} />
-      )}
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (

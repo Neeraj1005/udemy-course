a6941886346fab574a7dff780400f7c324b55d02
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-04-28 11:28:38 +0530

    add task added

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -17,6 +18,15 @@ function App() {
       reminder: true,
     },
   ]);
+
+  // Add Task
+  const addTask = (task) => {
+    // console.log(task)
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task }
+
+    setTasks([...tasks, newTask])
+  }
 
   // Delete Task
   const deleteTask = (id) => {
@@ -35,6 +45,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (

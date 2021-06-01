4411e58ce268561bda36e2721d7c8b1d8ac89115
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-06-01 21:08:41 +0530

    section 8 dashboard helper and auth user edit update evt

--- projectset1/next-fundamental/pages/events/edit/[id].js
+++ projectset1/next-fundamental/pages/events/edit/[id].js
@@ -1,4 +1,4 @@
-// import { parseCookies } from '@/helpers/index'
+import { parseCookies } from "@/helpers/index";
 import moment from "moment";
 import { FaImage } from "react-icons/fa";
 import { ToastContainer, toast } from "react-toastify";
@@ -13,7 +13,7 @@ import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/Form.module.css";
 
-export default function EditEventPage({ evt }) {
+export default function EditEventPage({ evt, token }) {
   const [values, setValues] = useState({
     name: evt.name,
     performers: evt.performers,
@@ -48,16 +48,16 @@ export default function EditEventPage({ evt }) {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
-        // Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(values),
     });
 
     if (!res.ok) {
-      // if (res.status === 403 || res.status === 401) {
-      //   toast.error("No token included");
-      //   return;
-      // }
+      if (res.status === 403 || res.status === 401) {
+        toast.error("No token included");
+        return;
+      }
       toast.error("Something Went Wrong");
     } else {
       const evt = await res.json();
@@ -182,20 +182,22 @@ export default function EditEventPage({ evt }) {
         <ImageUpload
           evtId={evt.id}
           imageUploaded={imageUploaded}
-          // token={token}
+          token={token}
         />
       </Modal>
     </Layout>
   );
 }
 
-export async function getServerSideProps({ params: { id } }) {
+export async function getServerSideProps({ params: { id }, req }) {
+  const { token } = parseCookies(req);
+
   const res = await fetch(`${API_URL}/events/${id}`);
   const evt = await res.json();
-
   return {
     props: {
       evt,
+      token,
     },
   };
 }

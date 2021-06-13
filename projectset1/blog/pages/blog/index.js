d61dc33f4104ab11a09f5819f241884cf93db4ef
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-06-13 13:37:28 +0530

    Blog content show

--- projectset1/blog/pages/blog/index.js
+++ projectset1/blog/pages/blog/index.js
@@ -1,8 +1,46 @@
-import Layout from "@/components/Layout"
-export default function BlogPage() {
-    return (
-        <Layout title="Blog Page">
-            <h1>BLog</h1>
-        </Layout>
-    )
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Layout from "@/components/Layout";
+import Post from "@/components/Post"
+import {sortByDate} from "@/utils/index"
+
+export default function BlogPage({ posts }) {
+  return (
+    <Layout>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts")); // get filenames in array under posts folder
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", ""); // convert filenames into slug
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+    
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+
+  });
+
+  return {
+    props: {
+      posts: posts.sort(sortByDate),
+    },
+  };
 }

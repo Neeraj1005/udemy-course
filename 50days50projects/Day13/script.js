286c853ba706a089b532ff5e8ee9fb21730c3b4d
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-06-18 19:03:38 +0530

    Day13 added

---
+++ 50days50projects/Day13/script.js
@@ -0,0 +1,95 @@
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+// Basic Tag element create done
+
+// textarea.addEventListener("keyup", (e) => {
+//   createTags(e.target.value);
+// });
+
+// function createTags(input) {
+//   const tags = input
+//     .split(",")
+//     .filter((tag) => tag.trim() !== "")
+//     .map((tag) => tag.trim());
+
+//   tagsEl.innerHTML = "";
+
+//   tags.forEach((tag) => {
+//     const tagEl = document.createElement("span");
+//     // Add class and text under tagEL
+//     tagEl.classList.add("tag");
+//     tagEl.innerText = tag;
+//     // Now append tagEl to inside tagsEl div
+//     tagsEl.appendChild(tagEl);
+//   });
+// }
+
+// More Advanced
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    // Add class and text under tagEL
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    // Now append tagEl to inside tagsEl div
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  const times = 30;
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
